ce02704f2721cc57082110a6c7e6683613b36b77
rasanga23 <rasanga.pramuddhika@overleap.tech>
2026-04-02 00:23:47 +0530

    Fix base path in Vite config to use a static repository name for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
-
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS && repoName ? `/${repoName}/` : '/',
+  base: '/TIME_LINE_OKIDOKI/',
 })
