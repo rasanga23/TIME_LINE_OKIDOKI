499a5608bb711968d8768eaa330c8f5e221c5f7b
rasanga23 <rasanga.pramuddhika@overleap.tech>
2026-04-03 14:28:16 +0530

    Remove outdated release dates from timeline data

--- src/timeline-data/release-dates.ts
+++ src/timeline-data/release-dates.ts
@@ -2,14 +2,6 @@ import type { ReleaseDateEntry } from './types'
 
 export const releaseDatesData: ReleaseDateEntry[] = [
   {
-    date: '2026-03-16',
-    capacityHours: 56,
-  },
-  {
-    date: '2026-03-30',
-    capacityHours: 56,
-  },
-  {
     date: '2026-04-20',
     capacityHours: 56,
   },
